dc005cb27cb492d0e5b4e4f7f40bd854da7243df
VolodymyrButs <wowabuz@gmail.com>
2020-05-20 18:40:02 +0300

    Updates

---
+++ precache-manifest.828c375f3934c375b32564a6d9485794.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cece638feeabeab0ecd3942a26f5d992",
+    "url": "/MSI-2020-Front-end-Development-Test/index.html"
+  },
+  {
+    "revision": "c68af1209d6993104825",
+    "url": "/MSI-2020-Front-end-Development-Test/static/css/main.62616655.chunk.css"
+  },
+  {
+    "revision": "403ad350d37d1b36b0e0",
+    "url": "/MSI-2020-Front-end-Development-Test/static/js/2.a4a43543.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MSI-2020-Front-end-Development-Test/static/js/2.a4a43543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c68af1209d6993104825",
+    "url": "/MSI-2020-Front-end-Development-Test/static/js/main.bf2fd7ea.chunk.js"
+  },
+  {
+    "revision": "0004f1234e00295811fb",
+    "url": "/MSI-2020-Front-end-Development-Test/static/js/runtime-main.c8d00197.js"
+  },
+  {
+    "revision": "679d6e7b3815277a380faf01b75c0f03",
+    "url": "/MSI-2020-Front-end-Development-Test/static/media/heart.679d6e7b.svg"
+  },
+  {
+    "revision": "f8a998d1878c0acd21eab998d9eead5c",
+    "url": "/MSI-2020-Front-end-Development-Test/static/media/link.f8a998d1.svg"
+  },
+  {
+    "revision": "ee31211cb3e3b4e4c50c3d68317da746",
+    "url": "/MSI-2020-Front-end-Development-Test/static/media/message.ee31211c.svg"
+  }
+]);
